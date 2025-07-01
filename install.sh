18a7a596e26497b866730a017f0e71e3b53ac07a
Vidya Galli <vidya.s.galli@.intel.com>
2025-07-01 23:32:41 +0000

    update for local debug

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 pip install -e .
 pip install -r examples/text-generation/requirements_lm_eval.txt
 pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.21.0
+export PYTHONPATH=$PYTHONPATH:/root/work/lm-evaluation-harness/
+cd /root/work/lm-evaluation-harness/ && pip install -e .
+cd /root/work/v_oh/examples/text-generation
